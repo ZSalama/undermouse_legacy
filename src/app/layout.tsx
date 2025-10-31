b72fcedff7748c277637cae2e50dc9328757bfee
Zack Salama <zacksalama@gmail.com>
2025-10-31 19:03:49 -0400

    remove posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,38 +2,38 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 // import NavBar from '@/components/NavBar/NavBar'
-import { PostHogProvider } from './providers'
+// import { PostHogProvider } from './providers'
 
 const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 })
 
 const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
-    title: 'UnderMouse',
-    description: 'Website for UnderMouse',
+	title: 'UnderMouse',
+	description: 'Website for UnderMouse',
 }
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode
+	children: React.ReactNode
 }>) {
-    return (
-        <html lang='en'>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <PostHogProvider>
-                    {/* <NavBar /> */}
-                    {children}
-                </PostHogProvider>
-            </body>
-        </html>
-    )
+	return (
+		<html lang='en'>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{/* <PostHogProvider> */}
+				{/* <NavBar /> */}
+				{children}
+				{/* </PostHogProvider> */}
+			</body>
+		</html>
+	)
 }
